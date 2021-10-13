3cd09e633ef7f1e253b9b5558288b2cac1f10fbe
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-10-13 07:32:48 +0100

    started styling the product display card

--- client/src/components/ProductDisplayCard/ProductDisplayCard.component.jsx
+++ client/src/components/ProductDisplayCard/ProductDisplayCard.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./ProductDisplayCard.styles.scss"
 import { BiAddToQueue } from "react-icons/bi";
-import { RiEdit2Line } from "react-icons/ri";
+import { RiEdit2Line, RiDeleteBin5Line } from "react-icons/ri";
 import { useHistory } from "react-router-dom"
 import DeleteStockItem from '../DeleteStockItem/DeleteStockItem.component';
 
@@ -14,22 +14,26 @@ const ProductDisplayCard = ({ shopData, productItems }) => {
                 shopData.map((sData) => (
                     <div className="productDisplayCard-category-container" key={sData._id}>
                         <div className="productDisplayCard-title">
-                            {sData.product} <span className="productDisplayCard-addIcon"><BiAddToQueue onClick={() => history.push(`/product/new/${sData._id}`)} /></span>
+                            <div className="productDisplayCard-titleItemContainer">
+                                <div>{sData.product}</div>
+                                <div className="productDisplayCard-icon"><BiAddToQueue onClick={() => history.push(`/product/new/${sData._id}`)} /></div>
+                            </div>
+                            <div className="productDisplayCard-icon"><RiDeleteBin5Line onClick={""} /></div>
                         </div>
                         <div className="productDisplayCard-item">
                             {
                                 productItems.map((item) => {
                                     if (sData._id === item.ProductCategoryId) {
                                         return (
-                                            <div className="productDisplayCard-itemName" key={item._id}>
+                                            <div className="productDisplayCard-card-container" key={item._id}>
                                                 <div className="productDisplayCard-iconContainer">
                                                     <span className="productDisplayCard-icon" onClick={() => history.push(`/product/update/${sData._id}/${item._id}`)}><RiEdit2Line /></span>
                                                     <DeleteStockItem idToDelete={item._id} />
                                                 </div>
-                                                <img src={item.imageUrl} alt="" />
-                                                <div>{item.itemName}</div>
-                                                <div>{item.price}</div>
-                                                <div>Quantity</div>
+                                                <img className="productDisplayCard-image" src={item.imageUrl} alt="" />
+                                                <div className="productDisplayCard-itemName">{item.itemName}</div>
+                                                <div className="productDisplayCard-price">{item.price}</div>
+                                                <div className="productDisplayCard-quantity">Quantity</div>
                                             </div>
                                         )
                                     }
