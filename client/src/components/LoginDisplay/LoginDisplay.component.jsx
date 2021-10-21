632cc1b3eff8af40ca7e388041c2803520afd6a1
Lee Macintyre <LeeMacintyre@LIVELOGIC.local>
2021-10-21 11:05:44 +0100

    test

--- client/src/components/LoginDisplay/LoginDisplay.component.jsx
+++ client/src/components/LoginDisplay/LoginDisplay.component.jsx
@@ -12,16 +12,16 @@ const LoginDisplay = () => {
         <div className="loginDisplay-container">
             <div className="loginDisplay-googleSignIn">
                 {/* <a href="http://localhost:8000/v1/google/auth/google"> */}
-                <a href="https://the-dealer-portal.herokuapp.com//v1/google/auth/google">
+                <a href="https://the-dealer-portal.herokuapp.com/v1/google/auth/google">
                     <CustomButton isGoogleSignIn>SignIn With Google</CustomButton>
                 </a>
                 <CustomButton>Sign In (not working yet)</CustomButton>
             </div>
-            {/* 
+
             <a href="http://localhost:8000/v1/google/auth/google">login </a>
             <a href="http://localhost:8000/v1/google/checklog"> check log </a>
             <a href="http://localhost:8000/v1/google/logout"> logout</a>
-            <a href="http://localhost:8000/v1/currentuser"> currentUser</a> */}
+            <a href="http://localhost:8000/v1/currentuser"> currentUser</a>
         </div>
     )
 }
