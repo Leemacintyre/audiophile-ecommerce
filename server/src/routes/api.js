3dc588e124bcebc9592e128826bb31ffda9ec70b
Lee Macintyre <LeeMacintyre@LIVELOGIC.local>
2021-10-21 10:15:15 +0100

    test

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,14 +3,14 @@ const { checkLoggedIn } = require("../middleware/checkLoggedIn");
 
 const currentUserRouter = require("./currentUser/currentUser.router");
 const googleAuthRouter = require("./googleAuth/googleAuth.router");
-const productCategoryRouter = require("./productCategory/productCategory.router");
-const productItemRouter = require("./productItem/productItem.router");
+// const productCategoryRouter = require("./productCategory/productCategory.router");
+// const productItemRouter = require("./productItem/productItem.router");
 
 const api = express.Router();
 
 api.use("/currentUser", currentUserRouter);
 api.use("/google", googleAuthRouter);
 // api.use("/products", productCategoryRouter);
-api.use("/productItem", productItemRouter);
+// api.use("/productItem", productItemRouter);
 
 module.exports = api;
